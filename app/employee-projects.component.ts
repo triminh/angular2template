01e513819e517a2b582ed5c75a34d1a1cfb19ee1
MinhTri <minh_tri585@yahoo.com>
2017-07-17 22:57:04 +0700

    child routes

---
+++ app/employee-projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'employee-projects-component',
+    template: `
+    <h3>This is projects</h3>
+    `
+})
+export class EmployeeProjectsComponent {
+
+}
